828723946cfad3b45899242e9e669eda8e7f5e74
suerovr <suerovr20@gamil.com>
2023-08-14 21:17:09 +0900

    dom 활용 html 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,19 @@
 "use strict";
 
-console.log("hello");
-console.log("bye")
+const id = document.querySelector("#id"),//선택자 id
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+console.log(id);
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+
+
+    };
+    console.log(req);
+};
